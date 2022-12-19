6ee8fc83a09196572f438e014781ccf7f1c5ec6c
kumuthu <ekumuthu@gmail.com>
2022-12-20 08:59:40 +1100

    comments

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
 from openai_write import get_poem
 
+# Create an instance of Flask.
 app = Flask(__name__)
 
 
+# Define the function with the path and method to call it.
 @app.get("/write_poem")
 def write_poem():
 
+    # Obtains the title from the request parameters.
     title = request.args['title']
 
-    result = {
-        "poem": get_poem(title)
-    }
+    # Writes the poem by calling the get_poem() method.
+    # It would make more sense to wrap the poem in a JSON if this endpoint is consumed by an app.
+    result = get_poem(title)
 
-    return result["poem"]
-
-
+    return result
--- openai_write.py
+++ openai_write.py
@@ -3,6 +3,7 @@ import openai
 
 def get_poem(title):
 
+    # Reads and sets the api key the first time this function is evoked.
     if not hasattr(get_poem, 'api_key'):
         api_key_file = open("api_key.txt", "r")
         get_poem.api_key = api_key_file.read()
@@ -10,13 +11,16 @@ def get_poem(title):
 
         openai.api_key = get_poem.api_key
 
+    # Builds prompt.
     prompt = f"Write a poem with the title '{title}'"
 
+    # Generates the response from GPT-3.
     response = openai.Completion.create(model="text-davinci-003",
                                         prompt=prompt,
                                         temperature=0.7,
                                         max_tokens=500)
 
+    # Extracts the poem.
     poem = response["choices"][0]["text"]
 
     return poem
