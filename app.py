28d98d37075a9ddc6787a18556afd0483eb2876c
kumuthu <ekumuthu@gmail.com>
2022-12-20 12:05:46 +1100

    progress

--- app.py
+++ app.py
@@ -1,19 +1,56 @@
 from flask import Flask, request
 from openai_write import get_poem
+import logging
 
 # Create an instance of Flask.
-app = Flask(__name__)
+app = Flask("poem-writer")
+
+# Setup logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+logger.addHandler(handler)
 
 
 # Define the function with the path and method to call it.
 @app.get("/write_poem")
 def write_poem():
 
+    # Initialize response.
+    response = {
+        "status": None,
+        "message": None,
+        "poem": None
+    }
+
     # Obtains the title from the request parameters.
-    title = request.args['title']
+    try:
+        title = request.args['title']
+        logger.info(f"Title received is '{title}'")
+    except KeyError as e:
+
+        logger.error("The parameter 'title' is missing in the request.")
+
+        response["status"] = "error"
+        response["message"] = "The parameter 'title' is missing in the request."
+
+        return response
 
     # Writes the poem by calling the get_poem() method.
-    # It would make more sense to wrap the poem in a JSON if this endpoint is consumed by an app.
-    result = get_poem(title)
+    try:
+        poem = get_poem(title, logger)
+    except Exception as e:
+        logger.error("Error during generating poem.")
 
-    return result
+        response["status"] = "error"
+        response["message"] = "Error during generating poem."
+
+        return response
+
+    # Build response.
+    response["status"] = "success"
+    response["message"] = "Poem has been successfully generated"
+    response["poem"] = poem
+
+    return response
--- openai_write.py
+++ openai_write.py
@@ -1,7 +1,8 @@
 import openai
+import json
 
 
-def get_poem(title):
+def get_poem(title, logger):
 
     # Reads and sets the api key the first time this function is evoked.
     if not hasattr(get_poem, 'api_key'):
@@ -15,13 +16,32 @@ def get_poem(title):
     prompt = f"Write a poem with the title '{title}'"
 
     # Generates the response from GPT-3.
-    response = openai.Completion.create(model="text-davinci-003",
-                                        prompt=prompt,
-                                        temperature=0.7,
-                                        max_tokens=500)
+    try:
+        response = openai.Completion.create(model="text-davinci-003",
+                                            prompt=prompt,
+                                            temperature=0.7,
+                                            max_tokens=500)
+
+        logger.info("Response received from OpenAI")
+        logger.debug("Response: " + json.dumps(response, indent=4))
+    except Exception as e:
+        logger.error("Exception during request to OpenAI.")
+        raise e
 
     # Extracts the poem.
-    poem = response["choices"][0]["text"]
+    try:
+        poem = response["choices"][0]["text"]
+    except KeyError as e:
+        logger.error("Response from GPT-3 has an unexpected format")
+        raise e
+
+    # Remove preceding and trailing whitespace.
+    poem = poem.strip()
+
+    # Checks if poem is empty.
+    if len(poem) == 0:
+        logger.error("Poem generated is empty")
+        raise Exception("Poem generated is empty")
 
     return poem
 
